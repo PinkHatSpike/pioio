066fe1b1511392bfa8a572388ad1d25f8c0b3f53
spike <spike@pinkhatproductions.com>
2013-07-01 15:47:26 -0700

    adding Eclipse workspace for compiling App-IOIO330 modified build process to reference IOIO .jar files built from Eclipse added build target to jar up IOIO PC class tree

--- src/App-IOIO0330/IOIOLib/target/android/gen/ioio/lib/BuildConfig.java
+++ src/App-IOIO0330/IOIOLib/target/android/gen/ioio/lib/BuildConfig.java
@@ -2,5 +2,5 @@
 package ioio.lib;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = false;
+    public final static boolean DEBUG = true;
 }
---
+++ src/App-IOIO0330/applications/IOIOSimpleApp/gen/ioio/examples/simple/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ioio.examples.simple;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
