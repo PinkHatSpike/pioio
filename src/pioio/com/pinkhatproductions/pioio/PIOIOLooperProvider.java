5c1db9b1c941026b7926b57f58dd16ccede599e8
Pink Hat Spike <spike@pinkhatproductions.com>
2013-07-16 16:20:23 -0700

    updating license text to LGPL 3.0

--- src/pioio/com/pinkhatproductions/pioio/PIOIOLooperProvider.java
+++ src/pioio/com/pinkhatproductions/pioio/PIOIOLooperProvider.java
@@ -1,18 +1,22 @@
 /*
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * Contributors:
- *     PinkHatSpike - spike@pinkhatproductions.com
- */
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, version 3.0.
+  (http://www.gnu.org/licenses/lgpl-3.0.html)
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+  
+  Contributors:
+      PinkHatSpike - spike@pinkhatproductions.com
+*/
  
 package com.pinkhatproductions.pioio;
 
